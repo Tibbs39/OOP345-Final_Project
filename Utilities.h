014e3c42c1d6e465bfe1631b345971954cd49553
Kenneth Yue <kyue3@myseneca.ca>
2018-11-14 11:30:47 -0500

    initial commit

---
+++ Utilities.h
@@ -0,0 +1,14 @@
+// Name: Kenneth Yue
+// Seneca Student ID: 127932176
+// Seneca email: kyue3@myseneca.ca
+// Date of completion: November 14, 2018
+//
+// I confirm that I am the only author of this file
+// and the content was created entirely by me.
+
+#ifndef UTILITIES_H
+#define UTILITIES_H
+
+
+
+#endif // UTILITIES_H
