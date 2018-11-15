207364b2a0d2ac55c5dc82043b67a205bd8bb406
Tibbs39 <kyue3@myseneca.ca>
2018-11-15 16:04:59 -0500

    implemented Utilities class

--- Utilities.h
+++ Utilities.h
@@ -8,7 +8,24 @@
 
 #ifndef UTILITIES_H
 #define UTILITIES_H
+#include <string>
 
+class Utilities {
+    size_t m_widthField = 1;
+    static char m_delimiter;
+
+    public:
+
+        void setFieldWidth(size_t);
+
+        size_t getFieldWidth() const;
+
+        const std::string extractToken(const std::string& str, size_t& next_pos, bool& more);
+
+        static void setDelimiter(const char&);
+
+        const char getDelimiter() const;
+};
 
 
 #endif // UTILITIES_H
